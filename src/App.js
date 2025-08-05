ec6353a4337f2c7c24189f4e42439a71756f4587
Safian62 <meharsafian62@gmail.com>
2025-08-05 13:31:30 +0500

    j

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginPage, SignUpPage, ActivationPage } from "./Routes.js";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Store from "./redux/store";
+import { loadUser } from "./redux/actions/user.js";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    Store.dispatch(loadUser());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route
+          path="/activation/:activation_token"
+          element={<ActivationPage />}
+        />
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import LoginPage from '../src/pages/Login'
+import SignUpPage from './pages/Signup'
+import ActivationPage from './pages/ActivationPage'
+
+
+
+
+export {
+    LoginPage,
+    SignUpPage,
+    ActivationPage
+}
